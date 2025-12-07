9916b0ba41559bb69f483da26c4a4d9b08da8a8d
clivet268 <clivet268@gmail.com>
2025-12-07 17:59:49 -0500

    Fixed issue of graphs having color of current HSPP state color swapped with last one _-_
    X axis is in msec and default window size is 1 sec

--- externalstart.sh
+++ externalstart.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+sudo echo "$(whoami)"
 # Run ssh {username}@{server} "externalstart.sh" from your machine
-if [[ ! -e /usr/sbin/clientruntest ]]; then
-	echo "clientruntest not present in sbin!"
+if [[ ! -e /usr/sbin/senderuntest ]]; then
+	echo "senderuntest not present in sbin!"
 	exit
 fi
 sudo whoami
-sudo clientruntest &
+sudo senderuntest
+sudo extractlogs
 exit
