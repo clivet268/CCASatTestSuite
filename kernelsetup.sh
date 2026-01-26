3ef3448f74389749cf91035175af50df7c71c406
clivet268 <clivet268@gmail.com>
2026-01-26 03:43:09 -0500

    kernel setup script additons

--- kernelsetup.sh
+++ kernelsetup.sh
@@ -1,5 +1,9 @@
 sudo apt-get update -y
-sudo apt-get install bison flex libncurses-dev libelf-dev elfutils libssl-dev bc -y
+sudo apt-get install bison flex libncurses-dev libelf-dev elfutils libssl-dev bc p7zip-full -y
+wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.14.tar.xz
+7z x linux-6.14.tar.xz
+7z x linux-6.14.tar
+cd linux-6.14/
 sudo cp /boot/config-$(uname -r) .config
 sudo find scripts -name "*.sh" -execdir chmod u+x {} +
 yes "" | sudo make -j$(nproc) bzImage
