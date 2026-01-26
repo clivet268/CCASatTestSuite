74b208ceed730886fd7ea5b21cdfd0859606cecb
clivet268 <clivet268@gmail.com>
2026-01-26 16:06:21 -0500

    Kernel setup finalized
    (cherry picked from commit ac368cdd8fa25c5ccdaae1cc35e82a61d75faab1)

--- kernelsetup.sh
+++ kernelsetup.sh
@@ -1,9 +1,11 @@
+sudo apt remove os-prober
 sudo apt-get update -y
 sudo apt-get install bison flex libncurses-dev libelf-dev elfutils libssl-dev bc -y
 sudo cp /boot/config-$(uname -r) .config
 sudo find scripts -name "*.sh" -execdir chmod u+x {} +
+sudo find . -type f ! -name "*.*" -execdir chmod u+x {} +
 yes "" | sudo make -j$(nproc) bzImage
 sudo make -j$(nproc) olddefconfig
 sudo make -j$(nproc)
 sudo make -j$(nproc) modules_install
-sudo make install
+sudo make install && sudo reboot
