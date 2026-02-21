98b5d2bb6bc88c2ef083e702b75ea5e203f68d24
clivet268 <clivet268@gmail.com>
2026-02-20 22:15:56 -0500

    change buffer sizes to support our BDP

--- recieverruntest.sh
+++ recieverruntest.sh
@@ -91,6 +91,16 @@ rmlock() {
 trap rmlock SIGINT
 trap rmlock SIGTERM
 
+
+
+sudo sysctl -w net.ipv4.tcp_window_scaling = 1
+sudo sysctl -w net.ipv4.tcp_rmem="26214400	26214400	26214400"
+sudo sysctl -w net.ipv4.tcp_wmem="26214400	26214400	26214400"
+sudo sysctl -w net.core.rmem_max="26214400"
+sudo sysctl -w net.core.wmem_max="26214400"
+sudo sysctl -w net.core.rmem_default="26214400"
+sudo sysctl -w net.core.wmem_default="26214400"
+
 #need sudo?
 #sudo echowname "running as : ${USER}"
 echowname "receiving ${numruns} time(s)..."
