0838a27901487d0938da39ea8922cb2abd91fe29
zaahmad <zaahmad@wpi.edu>
2026-02-21 13:15:42 -0500

    update jitter graphing

--- utility/jitter/graphJitter.py
+++ utility/jitter/graphJitter.py
@@ -1,43 +1,13 @@
-import matplotlib.pyplot as plt
-import numpy as np
+
 import os
 import argparse
 import parseCSV
+from parseCSV import numberType
 
-def graph(fiilename):
-    headers,data=parseCSV.parseTrace(fiilename)
+roundTripTimeIndex=1
+accumulatedTimeIndex=0
 
-    getRows = ["now_us",'bytes_acked',"rtt_us"]
-    cleaned = parseCSV.getRelevant(getRows,headers,data)
-    
-    # cleaner
-    m:dict[str,np._ArrayNumber_co] =dict()# list(map(lambda x,y: (x,y) ,getRows,cleaned))
-    for row in range(len( getRows)):
-        # m[getRows[row]] = np.array()
-        temp = []
-        for i in range(len(cleaned)):
-            temp.append(cleaned[i][row])
-        m[getRows[row]] = np.array(temp)
-    
-    m["now_us"] = np.vectorize(lambda x: float(x)/1000000)(m["now_us"])
-    m["rtt_us"] = np.vectorize(lambda x: float(x)/1000000)(m["rtt_us"])
-    # print(m["now_us"])
 
-    plt.suptitle(fiilename,fontsize=12)
-    
-    plt.subplot(3,1,1)
-    plt.plot(m["now_us"],m["rtt_us"])
-    plt.xlabel("Time (s)")
-    plt.ylabel("RTT (s)")
-    plt.title("RTT")
-    plt.grid()
-
-    plt.subplot(3,1,2)
-    
-    plt.plot(m["now_us"],m["bytes_acked"])
-    plt.xlabel("Time (s)")
-    plt.ylabel("Bytes ACKed")
-    plt.title("Throughput")
 
 def main():
     desc = '''
@@ -45,29 +15,9 @@ def main():
     '''
     parser =argparse.ArgumentParser(prog=os.path.basename(__file__),description=desc)
     parser.add_argument('fileName',type=str)
-    parser.add_argument('fileName2',type=str)
     args = parser.parse_args()
     
     
-    graph(args.fileName)
-    graph(args.fileName2)
-
-    # plt.grid()
-
-    #To double check that nothing got reordered
-    # plt.subplot(3,1,3)
-    # xx = []
-    # prev = -1
-    # for i in list(map(lambda x: x[0], enumerate(m["now_us"]))):
-    #     xx.append(i-prev)
-    #     prev = i
-    # plt.plot(xx)
-
-
-
-    plt.tight_layout()
-
-
     origName=str(os.path.basename(args.fileName))
     dirname = os.path.dirname(args.fileName)
     if(dirname):
@@ -75,6 +25,76 @@ def main():
         dirname = "jitterOutputImages"+os.sep+dirname
     os.makedirs(dirname,exist_ok=True)
     outFileName = f'{dirname}{origName}.png'
+
+    if(os.path.exists(outFileName) and os.path.getmtime(outFileName) > os.path.getmtime(args.fileName)):
+        print(f"\tSkipped: {outFileName} is newer than source file")
+        exit(0)
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    def calcInterpacketTime(t:list[tuple[numberType,numberType]]) -> list[numberType]:
+        outlist = [numberType(0)]
+        for i in range(1,len(t)):
+            diff = t[i][accumulatedTimeIndex]-t[i-1][accumulatedTimeIndex]
+            outlist.append(diff)
+        return outlist
+
+    def graph(fiilename):
+        headers,data=parseCSV.parseTrace(fiilename)
+
+        getRows = ["now_us",'bytes_acked',"rtt_us"]
+        cleaned = parseCSV.getRelevant(getRows,headers,data)
+        jrtt =calcInterpacketTime([(numberType(j[0]),numberType(j[2])) for j in cleaned])
+        
+        # cleaner
+        m:dict[str,np._ArrayNumber_co] =dict()# list(map(lambda x,y: (x,y) ,getRows,cleaned))
+        for row in range(len( getRows)):
+            # m[getRows[row]] = np.array()
+            temp = []
+            for i in range(len(cleaned)):
+                temp.append(cleaned[i][row])
+            m[getRows[row]] = np.array(temp)
+        
+        m["now_us"] = np.vectorize(lambda x: float(x)/1000000)(m["now_us"])
+        m["rtt_us"] = np.vectorize(lambda x: float(x)/1000000)(m["rtt_us"])
+        # print(m["now_us"])
+
+        plt.suptitle(fiilename,fontsize=12)
+        
+        plt.subplot(3,1,1)
+        plt.plot(m["now_us"],m["rtt_us"])
+        plt.xlabel("Time (s)")
+        plt.ylabel("RTT (s)")
+        plt.title("RTT")
+        plt.ylim(bottom=0)
+        plt.xlim(left=0)
+        plt.grid()
+
+        plt.subplot(3,1,2)
+        
+        plt.plot(m["now_us"],m["bytes_acked"])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Bytes ACKed")
+        plt.title("Throughput")
+        plt.ylim(bottom=0)
+        plt.xlim(left=0)
+
+        plt.subplot(3,1,3)
+        plt.plot(m["now_us"],jrtt)
+        plt.xlabel("Time (s)")
+        plt.ylabel("InterArrival Time (us)")
+        plt.title("Inter-Packet Arrival")
+        plt.ylim(bottom=0)
+        plt.xlim(left=0)
+    
+    graph(args.fileName)
+    graph(args.fileName2)
+
+
+    plt.tight_layout()
+
+
+    
     # print(outFileName)
     plt.savefig(outFileName,bbox_inches='tight')
     
