6820a48b7dadef3dba0feb430beff588fb3abaa1
clivet268 <clivet268@gmail.com>
2026-02-27 09:28:23 -0500

    added more deps after recent testing

--- dependencies.sh
+++ dependencies.sh
@@ -14,7 +14,9 @@ pip3 install iperf3
 pip3 install matplotlib
 pip3 install paramiko
 
-#if environment is externally managed 
+#if environment is externally managed
+sudo apt-get install iperf3
+sudo apt-get install make
 sudo apt install python3-matplotlib
 sudo apt install python3-paramiko
 sudo apt install openssh-server
