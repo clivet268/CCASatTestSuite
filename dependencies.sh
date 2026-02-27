c8ccbf84e1c5480ff3b5820bfba810aa36a34227
Thompson <csthompson@wpi.edu>
2026-02-27 09:14:39 -0500

    Cleanup and WIP help info

--- dependencies.sh
+++ dependencies.sh
@@ -19,5 +19,4 @@ sudo apt install python3-matplotlib
 sudo apt install python3-paramiko
 sudo apt install openssh-server
 sudo apt install openssh-client
-#sudo apt install tmux
 sudo apt-get install rsync
--- triggerremotetests_holding.sh
+++ triggerremotetests_holding.sh
@@ -24,18 +24,32 @@ recieverlocbind=""
 
 #set -o pipefail
 
-while getopts "dln:a:i:t:S:r:R:e:x:y:X:Y:" arg; do
+usage() {
+  cat <<EOF 
+${0##*/} - Script to be run by triggering machine, with a holding configuration
+    The holding configuration requires the trigger machine to be active during
+    the duration of the test but allows for timing to be controled by the trigger
+    machine.
+
+Usage: ${0##*/} [options]
+
+Options:
+  -h, --help    Display this help
+  -n <number of tets>   Input file
+  ...
+EOF
+}
+
+while getopts "lhn:a:i:t:S:r:R:e:x:y:X:Y:" arg; do
 	case $arg in
+		h)
+			echo "$(usage)"
+			exit
+			;;
 		a)
       		algorithm=$OPTARG
 			echo "Using the algorithm: ${algorithm}"
 			;;
-    	d)
-    		echo "Running with multiple terminals"
-    		echo "WARNING, this function is not up to date, will not work"
-    		exit
-    		terminalmode="desktop"
-    		;;
 		n) 	
     		numruns=$OPTARG
     		;;
@@ -66,7 +80,6 @@ while getopts "dln:a:i:t:S:r:R:e:x:y:X:Y:" arg; do
       		rangestring="$OPTARG"
 			;;
 		S)
-			#12:15
 			IFS='@'
       		read -ra senderstring <<< "$OPTARG"
 			senderuser=${senderstring[0]}
@@ -90,8 +103,8 @@ while getopts "dln:a:i:t:S:r:R:e:x:y:X:Y:" arg; do
 			IFS=' '
 			finalextract=" -e ${extractuser}@${extractip}"
 			;;
-	  *)
-	    echo "One or more flags not understood"
+		*)
+	    	echo "One or more flags not understood"
 	esac
 done
 
@@ -127,63 +140,41 @@ fi
 # if you setup keyauthentication for ssh this requires only entering sudo password
 #  on sender terminal, and entering pass for reciever (maybe)
 
+sssh="${senderuser}@${senderip}"
+rssh="${recieveruser}@${recieverip}"
 
-#this is outdated as most people will be using the cli only version
-#TODO update
-if [[ ${terminalmode} == "desktop" ]]; then
-	sssh="${senderuser}@${senderip}"
-	rssh="${recieveruser}@${recieverip}"
-	gnome-terminal -vvvv --disable-factory -- sh -c "ssh -tt ${sssh} "\''cd ${HOME}/CCASatTestSuite/; ${HOME}/CCASatTestSuite/senderuntest.sh'\'" -n ${numruns} -t ${rangestring} ${senderbind}${finalextract}; sleep 10" &
-	senderpid=$!
-
-
-	sleep 6s
-	read -p "[trigger] Enter to launch reciever terminal"$'\n' </dev/tty
-
-	gnome-terminal -vvvv --disable-factory -- sh -c "ssh -tt ${rssh} "\''cd ${HOME}/CCASatTestSuite/; ${HOME}/CCASatTestSuite/recieverruntest.sh'\'" -n ${numruns} -t ${r} -s ${senderip} ${recieverbind}${finalextract}; sleep 10" &
-	recieverpid=$!
+finalrange=""
+finaltime=""
+if [[ ${time} != "" ]]; then
+	finaltime="-t ${time} "
 else
-
-	sssh="${senderuser}@${senderip}"
-	rssh="${recieveruser}@${recieverip}"
-	
-	finalrange=""
-	finaltime=""
-	if [[ ${time} != "" ]]; then
-		finaltime="-t ${time} "
+	if [[ ${transfersize} = "" ]]; then
+		finaltime="-t 10 "
 	else
-		if [[ ${transfersize} = "" ]]; then
-			finaltime="-t 10 "
-		else
-			finalrange="-r ${rangestring} "
-		fi
+		finalrange="-r ${rangestring} "
 	fi
-	
-	#since we are holding and have no tty you will need to find another way to auth
-	#rn we have global timestamps with a long timestamp timeout,
-	#not ideal, has some security issue in theory, but works
-	
-	#TODO make nicer the vars all bunched up are flags that might not be present
-	#start remote sender
-	cmdstr="sudo -E -s bash -c "\'"cd /home/${senderuser}; /home/${senderuser}/CCASatTestSuite/senderuntest.sh -n ${numruns} -a ${algorithm} -i ${runid} ${finaltime}${finalrange}${senderbind}${finalextract} >> /home/${senderuser}/CCASatTestSuite/sender.out 2>&1 < /dev/null"\'
-	
-	#echo ${cmdstr}
-	ssh ${senderlocbind}${sssh} "${cmdstr}" &
-	
-	#wait for sender to get ready
-	echo "The sender has been triggered,"
-	sleep 6s
-	echo "Triggering receiver"
-	#start remote reciever
-	#cmdstr="sudo bash -c "\'"setsid nohup /home/${recieveruser}/CCASatTestSuite/recieverruntest.sh -n ${numruns} -t ${r} -s ${senderip} ${recieverbind} >> /home/${recieveruser}/CCASatTestSuite/reciever.out 2>&1 < /dev/null &  sleep 1000; exit"\'
-	cmdstr="sudo -E -s bash -c "\'"cd /home/${recieveruser}; /home/${recieveruser}/CCASatTestSuite/recieverruntest.sh -n ${numruns} -a ${algorithm} -i ${runid} ${finaltime}${finalrange} -s ${senderip} ${recieverbind}${finalextract} >> /home/${recieveruser}/CCASatTestSuite/reciever.out 2>&1 < /dev/null"\'
-
-	echo ${cmdstr}
-	ssh ${rssh} ${recieverlocbind}"${cmdstr}"
-	
-	#echo "This program will try to cleanup the remote after it ends,"
-	#echo "do Ctrl-C to kill it now or Ctrl-Z and bg to kill it later"
-	#one week timeout before you lose control, should we ever make it timeout???	
-	#sleep 604800s
 fi
+	
+#since we are holding and have no tty you will need to find another way to auth
+#rn we have global timestamps with a long timestamp timeout,
+#not ideal, has some security issue in theory, but works
+	
+#TODO make nicer the vars all bunched up are flags that might not be present
+#start remote sender
+cmdstr="sudo -E -s bash -c "\'"cd /home/${senderuser}; /home/${senderuser}/CCASatTestSuite/senderuntest.sh -n ${numruns} -a ${algorithm} -i ${runid} ${finaltime}${finalrange}${senderbind}${finalextract} >> /home/${senderuser}/CCASatTestSuite/sender.out 2>&1 < /dev/null"\'
+
+#echo ${cmdstr}
+ssh ${senderlocbind}${sssh} "${cmdstr}" &
+
+#wait for sender to get ready
+echo "The sender has been triggered,"
+
+sleep 6s
+
+echo "Triggering receiver"
+#start remote reciever
+cmdstr="sudo -E -s bash -c "\'"cd /home/${recieveruser}; /home/${recieveruser}/CCASatTestSuite/recieverruntest.sh -n ${numruns} -a ${algorithm} -i ${runid} ${finaltime}${finalrange} -s ${senderip} ${recieverbind}${finalextract} >> /home/${recieveruser}/CCASatTestSuite/reciever.out 2>&1 < /dev/null"\'
+
+#echo ${cmdstr}
+ssh ${rssh} ${recieverlocbind}"${cmdstr}"
 
