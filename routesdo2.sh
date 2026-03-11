6cbae86817801918e2ab567e35e6276e30b9f09d
clivet268 <clivet268@gmail.com>
2026-03-10 22:25:57 -0400

    cleanup

--- routesdo2.sh
+++
@@ -1,5 +0,0 @@
-sudo ip route replace default via 130.215.126.1 dev eth0
-sudo ip route replace 192.168.1.0/24 dev eth1 src 192.168.1.133 table nic2
-sudo ip route replace default via 192.168.1.1 dev eth1 table nic2
-
-sudo ip rule add from 192.168.1.133 table nic2
